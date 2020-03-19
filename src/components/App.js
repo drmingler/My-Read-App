1c4e485d944402f7e90df7727945e84f04049e94
“Drmingler” <kayymedia@gmail.com>
2020-03-19 20:58:11 +0100

    fix: Update Book shelf performance issue

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,18 @@ import React from "react";
 import "../App.css";
 import * as BooksApis from "../utils/BooksAPI";
 import Shelf from "./Shelf";
-import Search from "./Search";
 
 class App extends React.Component {
   state = {
     books: [],
-    search : []
-    // currentlyReading:[],
-    // wantToRead :[],
-    // read: []
+    search: []
     //<Shelf update={this.updateBook} books={this.state.books} />
   };
   componentDidMount() {
-    // BooksApis.update({id:"jAUODAAAQBAJ"},"read").then((data)=>(console.log(data)))
-    BooksApis.search("Fitness").then(data => {
-      console.log(data);
-    });
+    // BooksApis.update({id:"jAUODAAAQBAJ"},"read").then((data)=>(console.log(data.shelf)));
+    // BooksApis.search("Fitness").then(data => {
+    //   console.log(data);
+    // });
     // BooksApis.get("tsRhkvo80iUC").then(data => console.log(data));
     BooksApis.getAll().then(data => {
       this.setState(() => ({
@@ -25,40 +21,48 @@ class App extends React.Component {
       }));
     });
   }
-
+  
   updateBook = (book, shelf) => {
-    BooksApis.update(book, shelf).then(() =>
-      BooksApis.getAll().then(data => {
-        this.setState(() => ({
-          books: data
-        }));
-      })
-    );
+    BooksApis.update(book, shelf).then(() => {
+      book.shelf = shelf;
+      this.setState(prevState => ({
+        books: prevState.books
+          .filter(eachBookInShelf => eachBookInShelf.id !== book.id)
+          .concat([book])
+      }));
+    });
   };
 
-  searchBooks = (query) => {
+  searchBooks = query => {
     // If the search query is empty set the search state to an empty array
-    if(!query){
-      return this.setState(()=>({
+    if (!query) {
+      return this.setState(() => ({
         search: []
-      }))
+      }));
     }
     // Otherwise fetch the result from the server
+
     BooksApis.search(query).then(data => {
-      this.setState(()=>({
+      this.setState(() => ({
         search: !data || data.error ? [] : data
-      }))
-    })
+      }));
+    });
   };
 
   render() {
-    console.log(this.state);
+    console.log("Search ", this.state.search);
+    console.log("Books", this.state.books);
     return (
       <div>
-        <Search update={this.updateBook} queryList={this.state.search} searchBooks={this.searchBooks} />
+        <Shelf update={this.updateBook} books={this.state.books} />
       </div>
     );
   }
 }
 
 export default App;
+// <Search
+//     update={this.updateBook}
+//     queryList={this.state.search}
+//     searchBooks={this.searchBooks}
+// />
